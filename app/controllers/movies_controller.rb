34788b4ea8988176d96cd82b65739b02fcbadd94
EC2 Default User <ec2-user@ip-172-31-14-56.us-east-2.compute.internal>
2019-02-12 22:58:07 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
 
   def movie_params
+    puts "#{params}"
     params.require(:movie).permit(:title, :rating, :description, :release_date)
+    
   end
 
   def show
@@ -11,24 +13,38 @@ class MoviesController < ApplicationController
   end
 
   def index
+    puts "#{params}"
     @movies = Movie.all
+    #@selector = params[:sort]
+    if(params.has_key?(:sort))
+      @movies = @movies.order(params[:sort])
+    end
+    
+    
+    puts "#{@movies}"
   end
 
   def new
+    puts "#{params}"
     # default: render 'new' template
   end
 
   def create
+    puts "#{params}"
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
   def edit
+    puts "#{params}"
     @movie = Movie.find params[:id]
   end
-
+  def wrh
+    redirect_to :back
+  end
   def update
+    puts "#{params}"
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
@@ -36,6 +52,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
+    puts "#{params}"
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    def sort attri
+        @movies.sort_by! do|p|
+            p[attri]
+        end
+    end
+        
 end
